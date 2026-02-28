ca8e87385f29786f24e97f4e4a697e3b20e70450
ankita basarwadkar <ankitabasarwadkar@gmail.com>
2026-02-28 11:36:10 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route, Navigate } from "react-router"
+
+import Home from "./pages/Home"
+
+function App() {return (
+<Routes>
+        <Route path="/home" element={<Home />} />
+</Routes>
+)
+}
+
+export default App
