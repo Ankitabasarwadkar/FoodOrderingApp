82214f50df6946a2e5787c8a42e7d223f092b176
ankita basarwadkar <ankitabasarwadkar@gmail.com>
2026-03-01 10:25:32 +0530

    add .env and db.js file changes for common connction of db

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,13 +1,27 @@
 // config/db.js
 
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/foodOrderingDB");
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.error(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/foodOrderingDB");
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Atlas Connected Successfully");
   } catch (error) {
-    console.error(error.message);
+    console.error("MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
